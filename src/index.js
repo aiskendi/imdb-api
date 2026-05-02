e2faa6843661adc45fe5fbef27f302f42807197e
aiskendi <96066438+aiskendi@users.noreply.github.com>
2026-05-02 20:06:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import index from "./routes/index";
-import reviews from "./routes/reviews";
-import title from "./routes/title";
+import index from "./routes/index.js";
+import search from "./routes/search.js";
+import title from "./routes/title.js";
+import reviews from "./routes/reviews.js";
+import userRoutes from "./routes/user/index.js";
 import cache from "./helpers/cache";
-import search from "./routes/search";
-import userRoutes from "./routes/user";
 
 const app = new Hono();
 
@@ -18,4 +18,4 @@ app.route("/reviews", reviews);
 app.route("/user", userRoutes);
 app.route("/", index);
 
-app.fire();
+export default app;
