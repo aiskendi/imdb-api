9cfe19adf81d1417f3a76fdb416f3ce823bd9b0a
aiskendi <96066438+aiskendi@users.noreply.github.com>
2026-05-02 19:14:51 +0800

    Update cache.js

--- src/helpers/cache.js
+++ src/helpers/cache.js
@@ -1,36 +1,37 @@
-import config from "../../config";
+import config from "../../config.js";
 
-export default async function cache(c, next) {
+async function cache(c, next) {
   const key = c.req.url;
-  const cache = await caches.default;
-  const response = await cache.match(key);
+  const cacheStore = await caches.default;
+  const response = await cacheStore.match(key);
 
-  function getCacheTTL() {
+  function getCacheTL() {
     try {
       let url = key.toString().toLowerCase();
       if (url.includes("/reviews")) return 60 * 60 * 24;
       if (url.includes("/title")) return 60 * 60 * 24;
       if (url.includes("/search")) return 60 * 60 * 24 * 2;
     } catch (_) {}
-
     return 86400;
   }
 
   if (!response) {
     await next();
-
     if (c.res.status === 200 && !config.cacheDisabled) {
-      c.res.headers.append("Cache-Control", `public, max-age=${getCacheTTL()}`);
-      await cache.put(key, c.res.clone());
+      c.res.headers.append(
+        "Cache-Control",
+        `public, max-age=${getCacheTTL()}`
+      );
+      await cacheStore.put(key, c.res.clone());
     }
-
     return;
   } else {
-    // it was throwing Immutable error
-    for (let [key, value] of response.headers.entries()) {
-      c.res.headers.set(key, value);
+    for (let [k, value] of response.headers.entries()) {
+      c.res.headers.set(k, value);
     }
-
     return c.json(await response.json());
   }
 }
+
+export default cache;
+export { cache };
